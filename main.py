dd7e0bd41fc1ca8b1d16db2a90df43a04c8a2629
Romucmad <45262249+Romucmad@users.noreply.github.com>
2019-10-26 18:56:29 +0200

    Add files via upload
    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import random
+
+from highlightlib import  HighlightedDatafile
+from highlightlib import  Line
+from highlightlib import  combine
+test =  HighlightedDatafile("file.txt")
+
+
+
+
+lines = test.lines()
+
+myName = "Date Extractor"
+test.setName(myName)
+for line in lines:
+    toks = line.tokens()
+
+    if (toks[0] == "//"):
+        toks = combine(toks, 2, 3)
+        line.addRecord(0,1,2,3)
+        line.updateToken(toks)
+        test.addToList(line)
+
+    else:
+        toks = combine(toks, 1, 2)
+        line.addRecord(0,1, 2,3,4)
+        line.updateToken(toks)
+        test.addToList(line)
+
+
+test.create()
+#test.show()
+
