98d9a91a4e8b8d11e0d44c8d83742e1abb873abe
Romucmad <45262249+Romucmad@users.noreply.github.com>
2019-11-01 17:54:19 +0100

    Add files via upload

--- data_highlighter/main.py
+++ data_highlighter/main.py
@@ -1,1 +1,65 @@
+from highlighter import  HighlightedFile
 
+#NORMAL FILE
+dataFile = HighlightedFile('file.txt')
+
+# get the set of self-describing lines
+lines = dataFile.lines()
+
+for thisLine in lines:
+    tokens = thisLine.tokens()
+
+    # check the type
+    firstToken = tokens[0]
+    if firstToken.text == "//":
+        # event marker
+        eventImporter = "Simple Event importer"
+        dateToken = tokens[2]
+        dateToken.record(eventImporter, "Date", dateToken.text)
+        timeToken = tokens[3]
+        timeToken.record(eventImporter, "Time", timeToken.text)
+        eventToken = tokens[4]
+        eventToken.record(eventImporter, "Event", timeToken.text)
+
+        # and the whole=line record
+        thisLine.record(eventImporter, "Whole line")
+
+# output to file, display
+
+dataFile.export("out4.html")
+
+#CSV FILE
+
+
+
+dataFileCSV = HighlightedFile('file_comma.txt')
+
+# get the set of self-describing lines
+lines = dataFileCSV.lines()
+
+CSV_DELIM = "(?:,\"|^\")(\"\"|[\w\W]*?)(?=\",|\"$)|(?:,(?!\")|^(?!\"))([^,]*?)(?=$|,)|(\r\n|\n)"
+
+for thisLine in lines:
+
+    tokens = thisLine.tokens(CSV_DELIM, ",")  # note we specify delimiter
+
+
+    # check the type
+    firstToken = tokens[0]
+
+    if firstToken.text == "//":
+        # event marker
+        eventImporter = "Simple CSV Event importer"
+        dateToken = tokens[2]
+        dateToken.record(eventImporter, "Date", dateToken.text, "n/a")
+        timeToken = tokens[3]
+        timeToken.record(eventImporter, "Time", timeToken.text, "n/a")
+        eventToken = tokens[4]
+        eventToken.record(eventImporter, "Event", timeToken.text, "n/a")
+
+        # and the whole=line record
+        thisLine.record(eventImporter, "Whole line")
+
+# output to file, display
+dataFileCSV.export("out5.html")
+print("done")
