1484680837b94e889d938a62a9a03421e8ffd6fa
Romucmad <45262249+Romucmad@users.noreply.github.com>
2019-11-01 17:04:15 +0100

    Add files via upload

---
+++ src/Token.py
@@ -0,0 +1,35 @@
+from .CharArray import  SingleUsage
+class Token():
+    """
+    a single token
+    """
+
+    def __init__(self, span, text, lineStart, chars):
+        self.span = span
+        self.text = text
+        self.lineStart = lineStart
+        self.chars = chars
+
+    def __str__(self):
+        return "[(" + str(self.start()) + "-" + str(self.end()) + ")" + ":\"" + self.text + "\"]"
+
+    def start(self):
+        return self.lineStart + self.span[0]
+
+    def end(self):
+        return self.lineStart + self.span[1]
+
+    def record(self, tool: str, field: str, value: str, units: str = "n/a"):
+        """
+        Report on the usage of this token
+        Args:
+            tool(str):  name of the module handling the import
+            field(str): what the token is being interpreted as
+            value(str): what value the token provided
+            units(str): the units of the token
+        """
+        toolField = tool + "/" + field
+        message = "Value:" + str(value) + " Units:" + str(units)
+        for i in range(self.start(), self.end()):
+            usage = SingleUsage(toolField, message)
+            self.chars[i].usages.append(usage)
--- src/main.py
+++ src/main.py
@@ -1,73 +1,65 @@
-from highlib import  HighLight
+from src.HighlightedFile import  HighlightedFile
 
+#NORMAL FILE
+dataFile = HighlightedFile('file.txt')
 
-high=HighLight("file.txt",'RomanFile')
+# get the set of self-describing lines
+lines = dataFile.lines()
 
+for thisLine in lines:
+    tokens = thisLine.tokens()
 
+    # check the type
+    firstToken = tokens[0]
+    if firstToken.text == "//":
+        # event marker
+        eventImporter = "Simple Event importer"
+        dateToken = tokens[2]
+        dateToken.record(eventImporter, "Date", dateToken.text)
+        timeToken = tokens[3]
+        timeToken.record(eventImporter, "Time", timeToken.text)
+        eventToken = tokens[4]
+        eventToken.record(eventImporter, "Event", timeToken.text)
 
-high.record("Date", "ToolTip", 0,'yellow')
-high.record("Venicle", "n/a", 1,'yellow')
-high.record("Venicle", "n/a", 2,'blue')
-high.record("Speed", 'mp/a', 3,'white')
-high.record("Speed", 'mp/a', 4,'green')
+        # and the whole=line record
+        thisLine.record(eventImporter, "Whole line")
 
-high.recordEscape("Date", "ToolTip", 0,'yellow','//')
-high.recordEscape("Venicle", "n/a", 1,'blue','//')
-high.recordEscape("Venicle", "n/a", 2,'blue','//')
+# output to file, display
 
+dataFile.export("out4.html")
 
-
-
-
-high.create('out.html')
-
-
-
-
-
-
-
-
-
-
-
-
+#CSV FILE
 
 
 
+dataFileCSV = HighlightedFile('file_comma.txt')
+
+# get the set of self-describing lines
+lines = dataFileCSV.lines()
+
+CSV_DELIM = "(?:,\"|^\")(\"\"|[\w\W]*?)(?=\",|\"$)|(?:,(?!\")|^(?!\"))([^,]*?)(?=$|,)|(\r\n|\n)"
+
+for thisLine in lines:
+
+    tokens = thisLine.tokens(CSV_DELIM, ",")  # note we specify delimiter
 
 
+    # check the type
+    firstToken = tokens[0]
 
+    if firstToken.text == "//":
+        # event marker
+        eventImporter = "Simple CSV Event importer"
+        dateToken = tokens[2]
+        dateToken.record(eventImporter, "Date", dateToken.text, "n/a")
+        timeToken = tokens[3]
+        timeToken.record(eventImporter, "Time", timeToken.text, "n/a")
+        eventToken = tokens[4]
+        eventToken.record(eventImporter, "Event", timeToken.text, "n/a")
 
+        # and the whole=line record
+        thisLine.record(eventImporter, "Whole line")
 
-
-#from highlightlib import  HighlightedDatafile
-#from highlightlib import  combine
-
-#test =  HighlightedDatafile("file.txt")
-
-#lines = test.lines()
-
-#myName = "Date Extractor"
-#test.setName(myName)
-
-#for line in lines:
-#    toks = line.tokens()
-
-
-#    if (toks[0] == "//"):
-#        toks = combine(toks, 2, 3)
-#        line.addRecord(0,1,2,3)
-#        line.cover(0,[1,2,3])
-#        line.updateToken(toks)
-#        test.addToList(line)
-
-#    else:
-#        line.addRecord(0,1,2,4,5,6)
-#        line.updateToken(toks)
-#        test.addToList(line)
-
-
-#test.create()
-#test.show()
+# output to file, display
+dataFileCSV.export("out5.html")
 
