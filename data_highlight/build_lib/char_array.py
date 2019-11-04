ba9658038e238a07ed75df9d943e976a107abc8e
Romucmad <45262249+Romucmad@users.noreply.github.com>
2019-11-04 19:33:00 +0100

    Lib files

---
+++ data_highlight/build_lib/char_array.py
@@ -0,0 +1,23 @@
+class CharIndex:
+    """
+    storage structure used to describe the activity on a single character
+    """
+
+    def __init__(self, letter):
+        self.letter = letter
+        self.usages = []
+    def __str__(self):
+        message = "[" + self.letter + "]"
+        for usage in self.usages:
+            message += "(T/F:" + usage.toolField + ", msg:" + usage.message + ")"
+        return message
+
+
+class SingleUsage:
+    """
+    storage of a single activity
+    """
+
+    def __init__(self, toolField, message):
+        self.toolField = toolField
+        self.message = message
