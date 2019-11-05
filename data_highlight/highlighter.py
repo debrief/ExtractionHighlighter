27d6dcc7500c8fb4540e2929483512c66888df47
Romucmad <45262249+Romucmad@users.noreply.github.com>
2019-11-05 22:11:04 +0100

    Add files via upload
    Changed method splinted a bit added comment  to constructor. Think looks better. Changed syntax to PEP8

--- data_highlight/highlighter.py
+++ data_highlight/highlighter.py
@@ -1,41 +1,43 @@
-from .support.char_array import  CharIndex
-from .support.line import  Line
-from .support.color_picker import  hexColorFor, meanColorFor, colorFor
+from .support.char_array import CharIndex
+from .support.line import Line
+from .support.color_picker import hexColorFor, meanColorFor, colorFor
 
-class HighlightedFile():
+
+class HighlightedFile:
     """
     class that can load/tokenize a datafile, record changes to the file,
     then export a highlighted version of the file that indicates extraction
     """
 
-    def __init__(self, filename: str,numberOfLines=None):
+    def __init__(self, filename: str, number_of_lines=None):
         """
         Constructor for this object
         Args:
             filename (str): The name of the file to be parsed/reported upon
+            number_of_lines(int) Number of lines that should be showed  to the output
         """
         self.filename = filename
-        self.dictColor={}
-        self.numberOfLines=numberOfLines
+        self.dict_color = {}
+        self.number_of_lines = number_of_lines
+
     #
     def charsDebug(self):
         """
         Debug method, to check contents of chars
         """
         return self.chars
+
     def lines(self):
         """
         slice the file into single lines
         """
-        if self.numberOfLines == None:
-            return self.notLimitedLines()
-        elif self.numberOfLines <= 0 :
+        if self.number_of_lines is None:
+            return self.not_limited_lines()
+        elif self.number_of_lines <= 0:
             print("Negative number of lines. Please provide positive number")
             exit(1)
         else:
-            return self.limitedLines()
-
-
+            return self.limited_lines()
 
     def export(self, filename: str):
         """
@@ -47,112 +49,98 @@ class HighlightedFile():
 
         lastHash = ""
 
-        for charIndex in self.chars:
-            letter = charIndex.letter
+        for char_index in self.chars:
+            letter = char_index.letter
             thisHash = ""
             thisMessage = ""
             colors = []
-            for usage in charIndex.usages:
-                thisHash += usage.toolField
+            for usage in char_index.usages:
+                thisHash += usage.tool_field
                 needsNewLine = thisMessage != ""
-                colors.append(colorFor(usage.toolField,self.dictColor))
+                colors.append(colorFor(usage.tool_field, self.dict_color))
                 if needsNewLine:
                     thisMessage += " // "
-                thisMessage += usage.toolField + ", " + usage.message
+                thisMessage += usage.tool_field + ", " + usage.message
 
             # do we have anything to shade?
             if thisHash != "":
                 # generate/retrieve a color for this hash
-                newColor = meanColorFor(colors)
-                hexColor = hexColorFor(newColor)
+                new_color = meanColorFor(colors)
+                hex_color = hexColorFor(new_color)
 
                 # are we already in hash?
                 if lastHash != "":
                     # is it the different to this one?
-                    if (lastHash != thisHash):
+                    if lastHash != thisHash:
                         # ok, close the span
                         fOut.write("</span>")
 
                         # start a new span
-                        fOut.write("<span title='" + thisMessage + "' style=\"background-color:" + hexColor + "\"a>")
+                        fOut.write("<span title='" + thisMessage + "' style=\"background-color:" + hex_color + "\"a>")
                 else:
-                    fOut.write("<span title='" + thisMessage + "' style=\"background-color:" + hexColor + "\">")
+                    fOut.write("<span title='" + thisMessage + "' style=\"background-color:" + hex_color + "\">")
             elif lastHash != "":
                 fOut.write("</span>")
 
             # just check if it's newline
-            if (letter == "\n"):
-                fOut.write("</br>")
+            if letter == "\n":
+                fOut.write("<br>")
             else:
                 fOut.write(letter)
 
             lastHash = thisHash
 
-        if (lastHash != ""):
+        if lastHash != "":
             fOut.write("</span>")
 
         fOut.close()
 
-    def limitedLines(self):
+    def limited_lines(self):
         """
             If  numberOfLines were limited
         :return:
         """
 
         with open(self.filename, 'r') as file:
-            sampleLines = file.read()
+            sample_lines = file.read()
 
-        lineCtr = 0
-        lines = []
-        strLines = sampleLines.splitlines()
-        strLines= strLines[0:self.numberOfLines]
-        stringToChar = '\n'.join(str(e) for e in strLines)
-        self.chars = [None] * len(stringToChar)
-        # initialise the char index
-        charCtr = 0
-        for char in stringToChar:
-            # put letter into a struct
-            charInd = CharIndex(char)
-            self.chars[charCtr] = charInd
-            charCtr += 1
-        counter=0
+        str_lines = sample_lines.splitlines()
 
+        str_lines = str_lines[0:self.number_of_lines]
+        str_to_char = '\n'.join(str(e) for e in str_lines)
 
-        for thisLine in strLines:
-            thisLen = len(thisLine)
-            newL = Line(str(lineCtr), str(lineCtr + thisLen), thisLine, self.chars)
-            lines.append(newL)
-            lineCtr += thisLen + 1
-
-
+        lines = self.fill_char_array(str_to_char, str_lines)
         return lines
 
-    def notLimitedLines(self):
+    def not_limited_lines(self):
         with open(self.filename, 'r') as file:
-            sampleLines = file.read()
-
+            sample_lines = file.read()
 
             # make the char index the correct length
-        self.chars = [None] * len(sampleLines)
+        self.chars = [None] * len(sample_lines)
+
+        str_lines = sample_lines.splitlines()
+        lines = self.fill_char_array(sample_lines, str_lines)
+        return lines
+
+    def fill_char_array(self, string_to_char, array_to_lines):
+        line_ctr = 0
+        lines = []
+
+        self.chars = [None] * len(string_to_char)
 
         # initialise the char index
         charCtr = 0
-        for char in sampleLines:
+        for char in string_to_char:
             # put letter into a struct
             charInd = CharIndex(char)
             self.chars[charCtr] = charInd
             charCtr += 1
 
-            # ok, break the file into self-aware lines
-        lineCtr = 0
-        lines = []
-        strLines = sampleLines.splitlines()
-
-        stringToChar = ''.join(str(e) for e in strLines)
-        for thisLine in strLines:
-            thisLen = len(thisLine)
-            newL = Line(str(lineCtr), str(lineCtr + thisLen), thisLine, self.chars)
+        for this_line in array_to_lines:
+            thisLen = len(this_line)
+            newL = Line(str(line_ctr), str(line_ctr + thisLen), this_line, self.chars)
             lines.append(newL)
-            lineCtr += thisLen + 1
+            line_ctr += thisLen + 1
 
         return lines
