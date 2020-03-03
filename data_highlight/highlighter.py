5a46d38c03f9d702f7b94b1256be46ca7ef97f92
Robin Wilson <robin@rtwilson.com>
2020-03-03 12:26:25 +0000

    More docs etc for highlighter.py

--- data_highlight/highlighter.py
+++ data_highlight/highlighter.py
@@ -23,8 +23,6 @@ class HighlightedFile:
         self.dict_color = {}
         self.number_of_lines = number_of_lines
 
-    #
-
     def chars_debug(self):
         """
         Debug method, to check contents of chars
@@ -33,7 +31,7 @@ class HighlightedFile:
 
     def lines(self):
         """
-        slice the file into single lines
+        Slice the file into lines and return a list of Line objects
         """
         if self.number_of_lines is None:
             return self.not_limited_lines()
@@ -45,16 +43,19 @@ class HighlightedFile:
 
     def export(self, filename: str, include_key=False):
         """
-        Provide highlighter summary for this file
+        Provide highlighted summary for this file
         Args:
             filename (str): The name of the destination for the HTML output
+            include_key (bool): Whether to include a key at the bottom of the output
+            showing what each colour refers to
         """
         export_report(filename, self.chars, self.dict_color, include_key)
 
     def limited_lines(self):
         """
-            If  numberOfLines were limited
-        :return:
+        Return a list of Line objects for each line in the file,
+        producing only self.number_of_lines objects (to limit length
+        of output for very large files)
         """
 
         with open(self.filename, "r") as file:
@@ -70,6 +71,9 @@ class HighlightedFile:
         return lines
 
     def not_limited_lines(self):
+        """
+        Return a list of Line objects for each line in the file
+        """
         with open(self.filename, "r") as file:
             file_contents = file.read()
 
@@ -79,6 +83,10 @@ class HighlightedFile:
         return lines
 
     def fill_char_array(self, file_contents, lines_list):
+        """
+        Fill the array of characters and create individual Line objects
+        for each line, with appropriate references to the character array
+        """
         # Keeps track of which character in the file a line starts on
         line_start_counter = 0
         lines = []
@@ -91,14 +99,16 @@ class HighlightedFile:
 
         # For each line in the file create a Line object with a SmallToken
         # object as its child, keeping track of the length of the line
-        # and which character of the file this line starts on 
+        # and which character of the file this line starts on
         for this_line in lines_list:
             line_length = len(this_line)
             line_span = (0, len(this_line))
             # Create SmallToken object to keep track of the line length, the line itself
             # the start character of the line in the file, and a reference to the overall
             # list of characters
-            smallToken = SmallToken(line_span, this_line, int(line_start_counter), self.chars)
+            smallToken = SmallToken(
+                line_span, this_line, int(line_start_counter), self.chars
+            )
             new_l = Line([smallToken])
             lines.append(new_l)
             # Update the starting character of the line ready for next time
